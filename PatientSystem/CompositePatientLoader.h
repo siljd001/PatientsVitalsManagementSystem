1f5aa4c7b7d45352b29806bf2aca587968c1084d
a2962363 <a2962363@adelaide.edu.au>
2026-05-25 00:58:40 +0930

    FR2 Design Pattern: Composite -> New CompositePatientLoader

---
+++ PatientSystem/CompositePatientLoader.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "AbstractPatientDatabaseLoader.h"
+
+
+class CompositePatientLoader : public AbstractPatientDatabaseLoader {
+public:
+    // loaders and database connections soon :)
+
+};
