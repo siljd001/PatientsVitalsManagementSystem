7654b1a28481b7fa6e4bb09d7421ac22cce4a9be
a2962363 <a2962363@adelaide.edu.au>
2026-05-25 00:59:44 +0930

    CompositePatientLoader Class: added private variable called _loaders to store loaders

--- PatientSystem/CompositePatientLoader.h
+++ PatientSystem/CompositePatientLoader.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "AbstractPatientDatabaseLoader.h"
-
+#include <vector>
+#include <memory>
 
 class CompositePatientLoader : public AbstractPatientDatabaseLoader {
 public:
     // loaders and database connections soon :)
 
+private:
+    std::vector<std::unique_ptr<AbstractPatientDatabaseLoader>> _loaders;
 };
