f61aa77b942a05b4a20dd7f299da61e024ba74f9
a2962363 <a2962363@adelaide.edu.au>
2026-05-25 01:01:10 +0930

    CompositePatientLoader Class: addLoader() -> the loader to the composite loader,
    As well added inheritance from AbstractPatientDatabaseLoader

--- PatientSystem/CompositePatientLoader.h
+++ PatientSystem/CompositePatientLoader.h
@@ -6,7 +6,13 @@
 
 class CompositePatientLoader : public AbstractPatientDatabaseLoader {
 public:
-    // loaders and database connections soon :)
+	// Add a loader to the composite loader. The composite loader will take ownership of the loader and will be responsible for deleting it.
+    void addLoader(std::unique_ptr<AbstractPatientDatabaseLoader> loader);
+
+	// Inherit from AbstractPatientDatabaseLoader
+    void initialiseConnection() override;
+    void loadPatients(std::vector<Patient*>& patientIn) override;
+    void closeConnection() override;
 
 private:
     std::vector<std::unique_ptr<AbstractPatientDatabaseLoader>> _loaders;
