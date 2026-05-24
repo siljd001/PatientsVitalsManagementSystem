3d688832f1c2147021fec14f1ec458f59b89011d
a2962363 <a2962363@adelaide.edu.au>
2026-05-24 23:56:14 +0930

    Completed required members for PatientFileLoaderAdapter

---
+++ PatientSystem/PatientFileLoaderAdapter.cpp
@@ -0,0 +1,14 @@
+#include "PatientFileLoaderAdapter.h"
+
+void PatientFileLoaderAdapter::initialiseConnection()
+{
+	// no connection to initialise for file loading
+}
+void PatientFileLoaderAdapter::loadPatients(std::vector<Patient*>& patientIn)
+{
+	// TODO: FILE LOADER FOR patient.txt
+}
+void PatientFileLoaderAdapter::closeConnection()
+{
+	// no connection to close for file loading
+}
--- PatientSystem/PatientFileLoaderAdapter.h
+++ PatientSystem/PatientFileLoaderAdapter.h
@@ -7,5 +7,7 @@ class PatientFileLoaderAdapter : public AbstractPatientDatabaseLoader {
 private:
     PatientFileLoader _fileLoader;
 public:
-    
+    void initialiseConnection() override;
+    void loadPatients(std::vector<Patient*>& patientIn) override;
+    void closeConnection() override;
 };
