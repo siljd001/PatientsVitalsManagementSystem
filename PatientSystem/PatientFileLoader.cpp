6050e6dac1447ffdf45dd0588d9ce33f1f831b28
a2962363 <a2962363@adelaide.edu.au>
2026-05-25 00:09:39 +0930

    Analysed patient format and Implemented Patients.txt parsing

--- PatientSystem/PatientFileLoader.cpp
+++ PatientSystem/PatientFileLoader.cpp
@@ -14,11 +14,29 @@ using namespace std;
 
 std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file)
 {
-	vector<Patient*> patients{};
+    vector<Patient*> patients{};
 
     std::ifstream inFile(file);
+	// check if the file was opened successfully
     if (inFile.is_open()) {
-        // TODO: load your file here
+        std::string line;
+		// read the file line by line
+        while (std::getline(inFile, line)) {
+
+            // checks if line is empty if it is continue
+            if (line.empty()) continue;
+
+            // Split by '|'
+            std::stringstream ss(line);
+            std::string patientId, name, patientBirthday, diagnosis, patientVitals;
+
+			// the format analysed is: patientId, name, birthday, diagnosis, vitals
+            std::getline(ss, patientId, '|');
+            std::getline(ss, name, '|');
+            std::getline(ss, patientBirthday, '|');
+            std::getline(ss, diagnosis, '|');
+            std::getline(ss, patientVitals, '|');
+        }
     }
 
     return patients;
